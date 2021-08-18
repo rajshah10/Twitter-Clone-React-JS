c35c42432997b7270182d4df655912eba65cacd8
Raj shah <76859652+rajshah10@users.noreply.github.com>
2021-08-18 20:52:15 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,11 +1,6 @@
 import firebase from "firebase";
 const firebaseConfig = {
-    apiKey: "AIzaSyBVn7-E_RQBvddgFKfnljbiHhEwNlmDudU",
-    authDomain: "tweet-48868.firebaseapp.com",
-    projectId: "tweet-48868",
-    storageBucket: "tweet-48868.appspot.com",
-    messagingSenderId: "489908071649",
-    appId: "1:489908071649:web:fdd412670d638fc7081ba0"
+   //add yoy firebase config here
   };
 // Initialize firebase app  
 const firebaseApp=firebase.initializeApp(firebaseConfig);
